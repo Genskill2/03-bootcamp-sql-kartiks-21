36c3ce646cf1de75a4cc89ecd4f59509d0ff7067
Kartik Shukla <63971235+kartiks-21@users.noreply.github.com>
2021-06-18 13:41:58 +0530

    Update

---
+++ delete1.sql
@@ -0,0 +1,2 @@
+DELETE FROM books_subject WHERE SUBJECT = 9;
+DELETE FROM subjects WHERE name = 'History';
---
+++ update1.sql
@@ -0,0 +1,11 @@
+UPDATE publisher 
+
+SET
+
+name = 'Prentice Hall'
+
+WHERE
+
+country = 'India';
+
+SELECT * FROM publisher;
